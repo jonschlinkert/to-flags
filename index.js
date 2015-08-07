2ce2c38dc58566735febfe0a184bf9f8518dde0a
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-07 17:31:21 -0400

    update to use `to-flag`

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
 'use strict';
 
-var isObject = require('is-plain-object');
-var toArg = require('to-arg');
-
-/**
- * Generate an array of command line args from
- * the given `keys` or all options.
- *
- * ```js
- * var options = {
- *   foo: 'bar',
- *   abc: true,
- *   xyz: 10,
- *   one: false
- * };
- *
- * // create command line args for all options
- * toFlags(options);
- * //=> ['--foo=bar', '--abc', '--xyz=10', '--no-one']
- *
- *
- * // or specific options
- * toFlags(options, ['foo', 'abc']);
- * //=> ['--foo=bar', '--abc']
- * ```
- *
- * @param  {Object} `obj`
- * @param  {Array} `keys`
- * @return {Array} Array of args
- * @api public
- */
+var toFlag = require('to-flag');
 
 module.exports = function(obj, keys) {
-  if (!isObject(obj)) {
+  if (typeof obj !== 'object') {
     throw new TypeError('to-flags expects an object.');
   }
+
   keys = keys || Object.keys(obj);
   return keys.map(function (key) {
-    return toArg(key, obj[key]);
+    return toFlag(key, obj[key]);
   });
 };
